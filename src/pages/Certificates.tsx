a13db1d8e7aedc69bae711babc6f794fc1963eea
Thayna <thaynas.s502@gmail.com>
2023-06-27 00:34:39 -0300

    mudando o diretorio /public para o moodo root path

--- src/pages/Certificates.tsx
+++ src/pages/Certificates.tsx
@@ -3,13 +3,13 @@ import Certificate from '../component/Certificate'
 const Certificates = () => {
     return(
         <main>
-            <Certificate name={'Curso de Extensao - Programaçao Python UNIFACS'} href={'../../public/certificados/certificado_20230613220432.pdf'} />
+            <Certificate name={'Curso de Extensao - Programaçao Python UNIFACS'} href={'/certificados/certificado_20230613220432.pdf'} />
 
-            <Certificate name={'Google - Technical Support Fundamentals'} href={'../../public/certificados/Coursera ELN4HMHZHRKN.pdf'} />
+            <Certificate name={'Google - Technical Support Fundamentals'} href={'/certificados/Coursera ELN4HMHZHRKN.pdf'} />
 
-            <Certificate name={'Rocketseat - Funcionamento de computadores, programaçao, internet e mais.'} href={'../../public/certificados/discover-conectar-certificate.pdf'}/>
+            <Certificate name={'Rocketseat - Funcionamento de computadores, programaçao, internet e mais.'} href={'/certificados/discover-conectar-certificate.pdf'}/>
 
-            <Certificate name={'Rocketseat - O currículo de aprendizado inclui: HTML,CSS, JavaScript, NodeJS, SQL e mais'} href={'../../public/certificados/discover-fundamentar-certificate.pdf'} />
+            <Certificate name={'Rocketseat - O currículo de aprendizado inclui: HTML,CSS, JavaScript, NodeJS, SQL e mais'} href={'/certificados/discover-fundamentar-certificate.pdf'} />
         </main>
     )
 }
