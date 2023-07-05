ca55bbf192f6e39db70abfdcdbde3abd88f3f02f
Thayna <thaynas.s502@gmail.com>
2023-07-05 11:49:11 -0300

    mudando o style da pagina de redes

--- src/pages/SocialMedias.tsx
+++ src/pages/SocialMedias.tsx
@@ -1,10 +1,10 @@
 const SocialMedias = () => {
     return(
-        <>
-            <h3><img className="icons-programming" src="https://t.ctcdn.com.br/09Y6BbLFxNn7XGCYRGzEI0p0oy8=/400x400/smart/filters:format(webp)/i490027.jpeg" />Linkedin <a href="https://www.linkedin.com/in/thayss/">linkedin.com/in/thayss</a></h3>
+        <div className="socialmedias">
+            <h3><img className="icons-programming" src="https://t.ctcdn.com.br/09Y6BbLFxNn7XGCYRGzEI0p0oy8=/400x400/smart/filters:format(webp)/i490027.jpeg" />Linkedin <a className="link" href="https://www.linkedin.com/in/thayss/">www.linkedin.com/in/thayss</a></h3>
 
-            <h3><img className="icons-programming" src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" />Github <a href="https://github.com/ThaynaSantana">github.com/ThaynaSantana</a></h3>
-        </>
+            <h3><img className="icons-programming" src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" />Github <a className="link" href="https://github.com/ThaynaSantana">www.github.com/ThaynaSantana</a></h3>
+        </div>
     )
 }
 
