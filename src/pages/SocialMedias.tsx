53ec58f60ed393bd30df66b0900ee710f59f3727
Thayna Santana <88935936+ThaynaSantana@users.noreply.github.com>
2023-08-13 12:02:17 -0300

    adicionando target blank as redes sociasi

--- src/pages/SocialMedias.tsx
+++ src/pages/SocialMedias.tsx
@@ -1,9 +1,9 @@
 const SocialMedias = () => {
     return(
         <>
-            <h3><img className="icons-programming" src="https://t.ctcdn.com.br/09Y6BbLFxNn7XGCYRGzEI0p0oy8=/400x400/smart/filters:format(webp)/i490027.jpeg" />Linkedin <a href="https://www.linkedin.com/in/thayss/">linkedin.com/in/thayss</a></h3>
+            <h3><img className="icons-programming" src="https://t.ctcdn.com.br/09Y6BbLFxNn7XGCYRGzEI0p0oy8=/400x400/smart/filters:format(webp)/i490027.jpeg" />Linkedin <a target="_blank" href="https://www.linkedin.com/in/thayss/">linkedin.com/in/thayss</a></h3>
 
-            <h3><img className="icons-programming" src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" />Github <a href="https://github.com/ThaynaSantana">github.com/ThaynaSantana</a></h3>
+            <h3><img className="icons-programming" src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU" />Github <a target="_blank" href="https://github.com/ThaynaSantana">github.com/ThaynaSantana</a></h3>
         </>
     )
 }
